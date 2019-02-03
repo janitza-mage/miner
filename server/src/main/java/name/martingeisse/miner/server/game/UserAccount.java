099d9f2ea0471b37a8c35c6a5d1c1454f4c70f26
Martin Geisse <martingeisse@googlemail.com>
2019-02-03 21:12:34 +0100

    move account API to game network protocol, WIP

--- server/src/main/java/name/martingeisse/miner/server/game/UserAccount.java
+++ server/src/main/java/name/martingeisse/miner/server/game/UserAccount.java
@@ -4,19 +4,16 @@
  */
 package name.martingeisse.miner.server.game;
 
-import name.martingeisse.common.SecurityTokenUtil;
-import name.martingeisse.common.javascript.jsonbuilder.JsonListBuilder;
-import name.martingeisse.common.javascript.jsonbuilder.JsonObjectBuilder;
+import com.google.common.collect.ImmutableList;
+import name.martingeisse.miner.common.Faction;
 import name.martingeisse.miner.common.network.c2s.request.CreatePlayerRequest;
 import name.martingeisse.miner.common.network.c2s.request.DeletePlayerRequest;
 import name.martingeisse.miner.common.network.s2c.response.LoginResponse;
 import name.martingeisse.miner.common.util.UserVisibleMessageException;
 import name.martingeisse.miner.server.Databases;
-import name.martingeisse.miner.server.MinerServerSecurityConstants;
 import name.martingeisse.miner.server.postgres_entities.PlayerRow;
 import name.martingeisse.miner.server.postgres_entities.QPlayerRow;
 import name.martingeisse.miner.server.util.database.postgres.PostgresConnection;
-import org.joda.time.Instant;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -33,24 +30,15 @@ public final class UserAccount {
 	}
 
 	public LoginResponse getLoginResponse() {
-//		final QPlayerRow qp = QPlayerRow.Player;
-//		try (PostgresConnection connection = Databases.main.newConnection()) {
-//			List<LoginResponse.Element> responseElements = new ArrayList<>();
-//			for (PlayerRow player : connection.query().select(qp).from(qp).where(qp.userAccountId.eq(id), qp.deleted.isFalse()).fetch()) {
-//				responseElements.add(new LoginResponse.Element(player.getId(), player.getName(), player.getf));
-//				JsonObjectBuilder<?> playerBuilder = listBuilder.element().object();
-//				playerBuilder.property("id").number(player.getId());
-//				playerBuilder.property("name").string(player.getName());
-//				playerBuilder.property("faction").number(player.getFactionId());
-//				playerBuilder.end();
-//			}
-//			listBuilder.end();
-//			objectBuilder.end();
-//		}
-
-
-
-		throw new UserVisibleMessageException("NOT IMPLEMENTED");
+		final QPlayerRow qp = QPlayerRow.Player;
+		try (PostgresConnection connection = Databases.main.newConnection()) {
+			List<LoginResponse.Element> responseElements = new ArrayList<>();
+			for (PlayerRow playerRow : connection.query().select(qp).from(qp).where(qp.userAccountId.eq(id), qp.deleted.isFalse()).fetch()) {
+				responseElements.add(new LoginResponse.Element(playerRow.getId(), playerRow.getName(),
+					Faction.values()[playerRow.getFaction()], playerRow.getCoins()));
+			}
+			return new LoginResponse(ImmutableList.copyOf(responseElements));
+		}
 	}
 
 	public void createPlayer(CreatePlayerRequest request) {
