96bdf778b81a4ed4bdca82f0af5d1993bb59181d
Martin Geisse <martin.geisse@janitza.de>
2024-09-27 10:36:57 +0200

    ...

--- re-client/src/main/java/name/martingeisse/miner/re_client/sound/SoundTemplate.java
+++ re-client/src/main/java/name/martingeisse/miner/re_client/sound/SoundTemplate.java
@@ -1,22 +1,47 @@
 package name.martingeisse.miner.re_client.sound;
 
+import javax.sound.sampled.*;
 import java.io.IOException;
 import java.io.InputStream;
 
 /**
- * A static sound template, i.e. a fixed buffer with sound data, that can be played.
+ * A static sound template, i.e. a fixed buffer with sound data, that can be played. A single template can be re-used
+ * to play its sound multiple times, including multiple times at once (TODO this doesn't work yet -- see the play()
+ * method).
  */
-public final class SoundTemplate {
+public final class SoundTemplate implements AutoCloseable {
 
-    public static SoundTemplate loadClasspathOgg(Class<?> anchor, String path) throws IOException {
-        try (InputStream inputStream = anchor.getResourceAsStream(path)) {
-            // return AudioLoader.getAudio("OGG", inputStream);
-            return new SoundTemplate();
-        }
+    private final Clip clip;
+
+    private SoundTemplate(Clip clip) {
+        this.clip = clip;
     }
 
     public void play() {
-        // TODO
+        // TODO: this won't be able to play the same sound template multiple times at once -- which a template and
+        // its play() method are supposed to do -- but for now this is enough.
+        clip.setFramePosition(0);
+        clip.start();
+    }
+
+    @Override
+    public void close() {
+        clip.close();
+    }
+
+    public static SoundTemplate loadFromClasspath(Class<?> anchor, String path) {
+        try (InputStream inputStream = anchor.getResourceAsStream(path)) {
+            if (inputStream == null) {
+                throw new IOException("resource not found: " + path);
+            }
+            try (AudioInputStream ais = AudioSystem.getAudioInputStream(inputStream)) {
+                Clip clip = AudioSystem.getClip();
+                clip.open(ais);
+                return new SoundTemplate(clip);
+            }
+        } catch (Exception e) {
+            throw new RuntimeException("could not load sound file " + anchor + " / " + path, e);
+        }
     }
 
 }
