799e998bf0978107081f32029d0ae5d7670c1b3e
Martin Geisse <martin.geisse@janitza.de>
2024-09-27 11:13:04 +0200

    ...

--- re-client/src/main/java/name/martingeisse/miner/re_client/MyIntro.java
+++ re-client/src/main/java/name/martingeisse/miner/re_client/MyIntro.java
@@ -1,21 +1,59 @@
 package name.martingeisse.miner.re_client;
 
+import org.lwjgl.BufferUtils;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.InputStream;
+import java.nio.ByteBuffer;
 import java.nio.FloatBuffer;
 
 import static org.lwjgl.glfw.GLFW.*;
-import static org.lwjgl.glfw.GLFW.glfwSwapBuffers;
 import static org.lwjgl.opengl.GL.createCapabilities;
 import static org.lwjgl.opengl.GL11.GL_FLOAT;
 import static org.lwjgl.opengl.GL11.GL_TRIANGLES;
 import static org.lwjgl.opengl.GL11.GL_VERTEX_ARRAY;
 import static org.lwjgl.opengl.GL11.glDrawArrays;
-import static org.lwjgl.opengl.GL11.glEnableClientState;
-import static org.lwjgl.opengl.GL11.glVertexPointer;
+import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.opengl.GL11C.GL_LINEAR;
+import static org.lwjgl.opengl.GL11C.GL_RGBA;
+import static org.lwjgl.opengl.GL11C.GL_TEXTURE_2D;
+import static org.lwjgl.opengl.GL11C.GL_TEXTURE_MAG_FILTER;
+import static org.lwjgl.opengl.GL11C.GL_TEXTURE_MIN_FILTER;
+import static org.lwjgl.opengl.GL11C.GL_UNSIGNED_BYTE;
+import static org.lwjgl.opengl.GL11C.glBindTexture;
+import static org.lwjgl.opengl.GL11C.glGenTextures;
+import static org.lwjgl.opengl.GL11C.glTexImage2D;
+import static org.lwjgl.opengl.GL11C.glTexParameteri;
+import static org.lwjgl.opengl.GL15.GL_TEXTURE_COORD_ARRAY;
+import static org.lwjgl.opengl.GL15.glEnable;
+import static org.lwjgl.opengl.GL15.glTexCoordPointer;
 import static org.lwjgl.opengl.GL15.*;
-import static org.lwjgl.opengl.GL15.GL_STATIC_DRAW;
 import static org.lwjgl.system.MemoryUtil.*;
 
 public class MyIntro {
+
+    private static int convertToTexture(BufferedImage bufferedImage) {
+        int width = bufferedImage.getWidth();
+        int height = bufferedImage.getHeight();
+        ByteBuffer data = BufferUtils.createByteBuffer(width * height * 4);
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                int argb = bufferedImage.getRGB(x, y);
+                data.put((byte) (argb >> 16));
+                data.put((byte) (argb >> 8));
+                data.put((byte) argb);
+                data.put((byte) (argb >> 24));
+            }
+        }
+        data.flip();
+        int id = glGenTextures();
+        glBindTexture(GL_TEXTURE_2D, id);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+        return id;
+    }
 
     public static void main(String[] args) {
 
@@ -27,6 +65,21 @@ public class MyIntro {
         long window = glfwCreateWindow(800, 600, "MyIntro", NULL, NULL);
         glfwMakeContextCurrent(window);
         createCapabilities();
+
+        // load texture
+        int textureId;
+        {
+            BufferedImage bufferedImage;
+            try (InputStream inputStream = MyIntro.class.getResourceAsStream("/bricks1.png")) {
+                if (inputStream == null) {
+                    throw new RuntimeException("resource not found");
+                }
+                bufferedImage = ImageIO.read(inputStream);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+            textureId = convertToTexture(bufferedImage);
+        }
 
         // create and fill JVM buffer
         FloatBuffer buffer = memAllocFloat(3 * 2);
@@ -53,9 +106,15 @@ public class MyIntro {
             }
         });
 
+        // prepare texture
+        glEnable(GL_TEXTURE_2D);
+        glBindTexture(GL_TEXTURE_2D, textureId);
+
         // draw rotating triangle until stopped by the user
         glEnableClientState(GL_VERTEX_ARRAY);
         glVertexPointer(2, GL_FLOAT, 0, 0L);
+        glEnableClientState(GL_TEXTURE_COORD_ARRAY);
+        glTexCoordPointer(2, GL_FLOAT, 0, 0L);
         while (!glfwWindowShouldClose(window)) {
             glfwPollEvents();
             glDrawArrays(GL_TRIANGLES, 0, 3);
