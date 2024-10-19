87a7cf550683b15497ce8a22eb972673436c0fad
Martin Geisse <martin.geisse@janitza.de>
2024-10-19 08:27:01 +0200

    ...

--- re-client-2/src/main/java/name/martingeisse/miner/client/tools/KeyboardCodePrinter.java
+++ re-client-2/src/main/java/name/martingeisse/miner/client/tools/KeyboardCodePrinter.java
@@ -4,7 +4,7 @@
  * This file is distributed under the terms of the MIT license.
  */
 
-package name.martingeisse.miner.tools;
+package name.martingeisse.miner.client.tools;
 
 import name.martingeisse.gleng.Gleng;
 import name.martingeisse.gleng.GlengCallbacks;
