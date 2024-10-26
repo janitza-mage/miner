2b7decffc301edd1ebf25f6625b7856b05e893f5
Martin Geisse <martin.geisse@janitza.de>
2024-10-26 12:01:37 +0200

    ...

---
+++ re-client/src/main/java/name/martingeisse/miner/client/engine/gui/control/Control.java
@@ -0,0 +1,75 @@
+/**
+ * Copyright (c) 2010 Martin Geisse
+ * <p>
+ * This file is distributed under the terms of the MIT license.
+ */
+
+package name.martingeisse.miner.client.engine.gui.control;
+
+import com.google.common.collect.ImmutableList;
+import name.martingeisse.miner.client.engine.gui.GuiElement;
+import name.martingeisse.miner.client.engine.gui.GuiLogicFrameContext;
+
+/**
+ * Base class to build higher-level controls from primitive elements.
+ */
+public class Control extends GuiElement {
+
+	/**
+	 * the controlRootElement
+	 */
+	private GuiElement controlRootElement;
+
+	/**
+	 * Constructor.
+	 */
+	public Control() {
+	}
+
+	/**
+	 * Getter method for the controlRootElement.
+	 * @return the controlRootElement
+	 */
+	public final GuiElement getControlRootElement() {
+		return controlRootElement;
+	}
+
+	/**
+	 * Setter method for the controlRootElement.
+	 * @param controlRootElement the controlRootElement to set
+	 */
+	protected final void setControlRootElement(GuiElement controlRootElement) {
+		this.controlRootElement = controlRootElement;
+		if (controlRootElement != null) {
+			controlRootElement.notifyAddedToParent(this);
+		}
+		requestLayout();
+	}
+
+	@Override
+	public void handleLogicFrame(GuiLogicFrameContext context) {
+		controlRootElement.handleLogicFrame(context);
+	}
+
+	@Override
+	public void handleGraphicsFrame() {
+		controlRootElement.handleGraphicsFrame();
+	}
+
+	@Override
+	public final void requestSize(int width, int height) {
+		controlRootElement.requestSize(width, height);
+		setSize(controlRootElement.getWidth(), controlRootElement.getHeight());
+	}
+
+	@Override
+	protected final void onAbsolutePositionChanged(int absoluteX, int absoluteY) {
+		controlRootElement.setAbsolutePosition(absoluteX, absoluteY);
+	}
+
+	@Override
+	public final ImmutableList<GuiElement> getChildren() {
+		return ImmutableList.of(controlRootElement);
+	}
+
+}
